9afe98fe2027757decbdb41bb6f7aa14c58f1e72
CodingIsFood <ayoodedere@gmail.com>
2019-02-24 00:07:21 +0100

    Updates

---
+++ precache-manifest.aab3935c071381107fc22b43d4a01b7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f51029a8c10f9c161ff",
+    "url": "/weatherApp/static/css/main.226e5b50.chunk.css"
+  },
+  {
+    "revision": "8f51029a8c10f9c161ff",
+    "url": "/weatherApp/static/js/main.8f51029a.chunk.js"
+  },
+  {
+    "revision": "c7ccdfe4aa37150b2ddb",
+    "url": "/weatherApp/static/js/runtime~main.c7ccdfe4.js"
+  },
+  {
+    "revision": "baa43413566b01abd340",
+    "url": "/weatherApp/static/js/2.baa43413.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/weatherApp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/weatherApp/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "fea3ee08721501588ff0ee15c786cd06",
+    "url": "/weatherApp/index.html"
+  }
+];
