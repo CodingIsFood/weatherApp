67805b1301ee6ba5fa3155e9aa431ddafb35542a
CodingIsFood <ayoodedere@gmail.com>
2019-06-28 10:11:38 +0100

    Updates

---
+++ precache-manifest.b3e9d2c7d463af859396f9b3b8b9a7fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e45690a9ad6a598d42a",
+    "url": "/static/css/main.5ff9ba84.chunk.css"
+  },
+  {
+    "revision": "9e45690a9ad6a598d42a",
+    "url": "/static/js/main.9e45690a.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "baa43413566b01abd340",
+    "url": "/static/js/2.baa43413.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "276f2fddb464d4159fbbec5e8287fe0c",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/weatherApp/precache-manifest.aab3935c071381107fc22b43d4a01b7e.js"
+  "/precache-manifest.b3e9d2c7d463af859396f9b3b8b9a7fd.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/weatherApp/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
